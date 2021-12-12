da68f1d437c487edce9dcf652c0b7653b9e98943
kuwabara-hirokazu <h.kuwabara1103@gmail.com>
2021-12-13 00:30:32 +0900

    ApolloClientを設定

---
+++ app/src/main/java/com/example/apollo_android_github/data/repository/GithubRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.apollo_android_github.data.repository
+
+class GithubRepositoryImpl {
+}
--- app/src/main/java/com/example/apollo_android_github/di/DataModule.kt
+++ app/src/main/java/com/example/apollo_android_github/di/DataModule.kt
@@ -5,6 +5,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.apollographql.apollo.ApolloClient
+import com.example.apollo_android_github.BuildConfig
 import com.example.apollo_android_github.data.source.local.PreferencesData
 import com.example.apollo_android_github.data.source.local.PreferencesDataImpl
 import dagger.Module
@@ -12,6 +14,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +25,41 @@ class DataModule {
 
     companion object {
         private const val DATA_STORE_KEY = "storeKey"
+        private const val Authorization_KEY = "Authorization"
+        private const val GITHUB_SERVER_URL = "https://api.github.com/graphql"
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthorizationInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader(Authorization_KEY, "Bearer ${BuildConfig.GITHUB_KEY}")
+                .build()
+            chain.proceed(request)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApolloClient(okHttpClient: OkHttpClient): ApolloClient {
+        return ApolloClient.builder()
+            .serverUrl(GITHUB_SERVER_URL)
+            .okHttpClient(okHttpClient)
+            .build()
     }
 
     @Singleton
