14c2567ee5d41d99610535cdd6a87cbceff0ff6b
kuwabara-hirokazu <h.kuwabara1103@gmail.com>
2021-12-28 00:36:07 +0900

    エラーハンドリングを追加

--- app/src/main/java/com/example/apollo_android_github/ext/FailureExt.kt
+++ app/src/main/java/com/example/apollo_android_github/ext/FailureExt.kt
@@ -1,12 +1,28 @@
 package com.example.apollo_android_github.ext
 
+import com.apollographql.apollo3.exception.ApolloHttpException
 import com.example.apollo_android_github.R
 import com.example.apollo_android_github.view.ui.Failure
 
 fun Failure.toMessage(): Int {
     return when (execution) {
-        else -> when (error) {
-            else -> R.string.error_message_default
-        }
+        else -> error.toMessage()
+    }
+}
+
+fun Throwable.toMessage(): Int {
+    return when (this) {
+        is ApolloHttpException -> this.toMessage()
+        else -> R.string.error_message_default
+    }
+}
+
+fun ApolloHttpException.toMessage(): Int {
+    return when (statusCode) {
+        401 -> R.string.error_message_401
+        500 -> R.string.error_message_500
+        503 -> R.string.error_message_503
+        504 -> R.string.error_message_504
+        else -> R.string.error_message_default
     }
 }
--- app/src/main/java/com/example/apollo_android_github/ext/FragmentExt.kt
+++ app/src/main/java/com/example/apollo_android_github/ext/FragmentExt.kt
@@ -5,11 +5,16 @@ import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.example.apollo_android_github.R
+import com.example.apollo_android_github.data.source.remote.ApolloApiException
 import com.example.apollo_android_github.view.ui.Failure
 import com.google.android.material.snackbar.Snackbar
 
 fun Fragment.showError(view: View, failure: Failure) {
-    Snackbar.make(view, failure.toMessage(), Snackbar.LENGTH_LONG)
+    val message = when (failure.error is ApolloApiException) {
+        true -> failure.error.messages.joinToString { it }
+        false -> getString(failure.toMessage())
+    }
+    Snackbar.make(view, message, Snackbar.LENGTH_LONG)
         .setAction(R.string.retry) { failure.retry() }
         .show()
 }
